a17af8e2b4ade7c1b30eaf8f58a8008264d767f4
kennymaccormak <volodymyr.chygura@gmail.com>
2016-07-14 16:50:01 +0300

    continue adding media style

---
+++ js/media800.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+        if ($(window).width() <= '800'){
+                $('.stuff').addClass('newStuff');
+                $('.stuff').removeClass('stuff');
+                $('.stuff_foto').addClass('newStuff_foto');
+                $('.team_left').removeClass('team_left');
+                $('.team_right').removeClass('team_right');
+                $('.team_center').removeClass('team_center');
+                $('.stuff_foto').removeClass('stuff_foto');
+        };
+
+});
+
+
+
+
