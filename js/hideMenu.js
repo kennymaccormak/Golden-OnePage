e62b904018f8784e3d6a82d6c589cedabb8571dd
kennymaccormak <volodymyr.chygura@gmail.com>
2016-07-18 22:27:00 +0300

    change hideMenu.js file

--- js/hideMenu.js
+++ js/hideMenu.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
+var hideMenu = function () {
 
-    $('.btn-hide_menu').click(function() {
+    $('.btn-hide_menu').click(function () {
         var $hideMenu = $(this);
-        console.log('$hideMenu:', $hideMenu);
 
         if(parseInt($hideMenu.val()) == 0){
             $('.hideMenu').animate({right: "0px"}, 200);
-            $('body').animate({right: "200px"}, 200);
+            $hideMenu.animate({right: "200px"}, 200);
             $hideMenu.addClass('btn-show_menu');
             $hideMenu.val(1);
         }
         else {
             $('.hideMenu').animate({right: "-200px"}, 200);
-            $('body').animate({right: "0px"}, 200);
+            $hideMenu.animate({right: "0px"}, 200);
             $hideMenu.removeClass('btn-show_menu');
             $hideMenu.val(0);
         }
     });
-});
+};
 
+$(document).ready(hideMenu);
